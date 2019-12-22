9764a704a159e2eae19e9aec64b4d5fb0a0435ed
二渡杏 <futawatariann@niwatariannoMacBook-Pro.local>
2019-12-22 12:42:47 +0900

    init

---
+++ sapuri/SupporterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SupporterViewController.swift
+//  sapuri
+//
+//  Created by 二渡杏 on 2019/12/16.
+//  Copyright © 2019 二渡杏. All rights reserved.
+//
+
+import UIKit
+
+class SupporterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func back() {
+        self.dismiss(animated: true, completion: nil)
+    }
+        // Do any additional setup after loading the view.
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ sapuri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sapuri
+//
+//  Created by 二渡杏 on 2019/12/15.
+//  Copyright © 2019 二渡杏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
